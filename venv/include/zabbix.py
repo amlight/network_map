43bcc3db1be7ba8633212095bc53a031ef4262e0
Arturo <ArturoQuintana@users.noreply.github.com>
2019-03-14 16:55:34 -0400

    Delete zabbix.py
    not need it

--- venv/include/zabbix.py
+++
@@ -1,5 +0,0 @@
-class Zabbix:
-    def __init__(self):
-        self.ZABBIX_SERVER = 'https://ultralog.ampath.net/zabbix'
-        self.ZABBIX_USER = 'apiuser'        # "apiuser"
-        self.ZABBIX_PSSW = 'Ciaraapiuser*'  # "Ciaraapiuser*"
